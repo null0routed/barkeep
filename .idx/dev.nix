e7618dfa5fe232faacff10745584c2fdcde090d6
null0routed <81880083+null0routed@users.noreply.github.com>
2025-04-29 18:45:25 +0000

    Updated dev.nix

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -2,15 +2,10 @@
   channel = "stable-24.05";
   packages = [
     pkgs.nodejs_20
-    pkgs.pinentry
     pkgs.gnupg
+    pkgs.pinentry-tty
     pkgs.git
   ];
-  programs.gnupg.agent = {
-    enable = true;
-    enableSSHSupport = true;
-  };
-  services.pcscd.enable = true;
   idx.extensions = [
     "vscode.npm"
   ];
